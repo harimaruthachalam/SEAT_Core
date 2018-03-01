8a2be721176331513cee9d0e8f832d94396ddbda
Hari Maruthachalam <srihari.m.srihari@gmail.com>
2018-03-01 12:47:05 +0530

    Other options in File Upload

--- fileUploader.php
+++ fileUploader.php
@@ -5,7 +5,17 @@
 </head>
 <body>
   <form enctype="multipart/form-data" action="fileUploader.php" method="POST">
-    <p>Upload your file</p>
+    <p>What to compute?</p>
+    <input type="radio" name="compute" value="s" checked="checked">Stable matching</input>
+    <input type="radio" name="compute" value="p">Maximum cardinality popular matching</input>
+    <input type="radio" name="compute" value="m">Maximum cardinality matching</input>
+    <input type="radio" name="compute" value="h">Matching in a given HRLQ instance</input>
+    <br/>
+    <p>Who is proposing the algorithm?</p>
+    <input type="radio" name="proposing" value="A" checked="checked">Resident proposing algorithm</input>
+    <input type="radio" name="proposing" value="B">Hospital proposing algorithm</input>
+    <br/>
+      <p>Upload your file</p>
     <input type="file" name="uploaded_file"></input><br />
     <input type="submit" value="Upload"></input>
   </form>
@@ -18,7 +28,8 @@
     system('rm '.$path.'*');
     $path = $path . basename( $_FILES['uploaded_file']['name']);
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-      $command = 'algo/GraphMatching/build/graphmatching -B -s -i ';
+      // $command = 'algo/GraphMatching/build/graphmatching -B -s -i ';
+      $command = 'algo/GraphMatching/build/graphmatching -'.$_POST['proposing'].' -'.$_POST['compute'].' -i ';
       $command = $command.$path;
       $command = $command.' -o ';
       $command = $command.'temp/output.txt';
