f15bd2b6e90ea0a19dbf46a6762500050e5214b1
Hari Maruthachalam <srihari.m.srihari@gmail.com>
2018-03-07 16:44:08 +0530

    Feeback to text file

--- contact.php
+++ contact.php
@@ -23,15 +23,46 @@
 
     <?php include 'header.php' ?>
 
-
-    <section id="contact" class="bg-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-            <h3>Contact us at </h3>
-    <p> seat@wmail.iitm.ac.in
+        <section id="contact" class="bg-light">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <?php
+                // POST method part
+                  if(isset($_POST['contactName']))
+                  {
+                    try{
+                      // Currently, we are writing to a file. Later modify the code to enable the php engine to send a email.
+                      $fp = fopen( "temp/feedback.txt", "a" );
+                      date_default_timezone_set('Asia/Kolkata');
+                      $timestamp = date("Y-m-d H:i:s");
+                      fputs( $fp, $_POST['contactName']."\tEncryption(".$_POST['contactEmail'].")\t".$_POST['contactFeedback']."\t".$timestamp."\n" );
+                      fclose( $fp );
+                    }
+                    catch(Exception $e)
+                    {
+                      echo "Error Writing to the file!";
+                      exit(-1);
+                    }
+                  echo "<h2>Feedback sent successfully!</h2>";
+                  }
+                ?>
+<form method="post" action="contact.php">
+            <h3>Send us feedback</h3>
+            <table border="0">
+              <tr><td>Name</td><td>&nbsp;&nbsp;&nbsp;</td><td><input type="text" name="contactName" placeholder="Enter your Name" /></td></tr>
+              <tr><td></td><td></td><td></td></tr>
+              <tr><td>Email Address</td><td>&nbsp;&nbsp;&nbsp;</td><td><input type="email" name="contactEmail" placeholder="Enter your Email Address" /></td></tr>
+              <tr><td></td><td></td><td></td></tr>
+              <tr><td>Feedback</td><td>&nbsp;&nbsp;&nbsp;</td><td><textarea name="contactFeedback" placeholder="Enter your Feedback"></textarea></td></tr>
+              <tr><td></td><td></td><td></td></tr>
+              <tr><td></td><td><input type="submit" value="Send" />&nbsp;&nbsp;&nbsp;<input type="reset" value="Clear" /></td><td></td></tr>
+            </table>
+<br/>
+    <p>The feedback will sent to <strong><em>seat [AT] wmail [DOT] iitm [DOT] ac [DOT] in</em></strong>
 </p>
 
+</form>
 			</div>
         </div>
       </div>
