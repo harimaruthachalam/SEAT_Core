72add9a4efcbfb491a5775a98b9dce5fde7dc60b
Srihari Maruthachalam <srihari.m.srihari@gmail.com>
2018-11-26 12:42:39 +0530

    Max Credit - minor change

--- services/Constants.java
+++ services/Constants.java
@@ -7,28 +7,28 @@ package services;
  * 2. Department criteria constants eg. #define Humanities 3
  */
 public class Constants{
-	
+
 	//Ranking criteria constants
 	public static final int random = 1;
 	public static final int stratifiedRandom = 2;
 	public static final int CGPA = 3;
-	
+
 	//Output file names
 	public static final String inputDataErrorLogFileName = "InputDataErrorLog.txt";
 	public static final String slotOrderingOutputFileName = "SlotOrderingUsed";
-	
+
 	//Inside and outside department suffixes
 	public static final String insideDepartmentSuffix = "$inside";
 	public static final String outsideDepartmentSuffix = "$outside";
-	
+
 	//String values for Core,Elective and Backlog courses
 	public static final String coreCourse = "CORE";
 	public static final String electiveCourse = "ELEC";
 	public static final String backlogCourse = "BACKLOG";
-	
+
 	//Default limit on the maxCredits for a student, if the department has not provided any
-	public static final int maxCreditDefaultLimit = 72;
-	
+	public static final int maxCreditDefaultLimit = 81;
+
 	//Colour code for the 'no colour' option
 	public static final int noColourOption = 0;
 }
